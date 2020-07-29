be5dbbc078d48a84074d038a778d2ef3dcad093a
a.rybski <a.rybski@techocean.pl>
2020-07-29 09:56:49 +0200

    Adding weak plot creation Updated dataframe

--- data_LBM.py
+++ data_LBM.py
@@ -98,6 +98,16 @@ def PlotCreator():
     GPU_load = (df1['Total size'] / df1['Devices']).unique().tolist()
     Filt_GPU_load = [(df1['Total size'] / df1['Devices'] == load) for load in GPU_load]
 
+    wd = os.getcwd()
+
+    dirName= os.path.join(wd, 'plots')
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
+        print("Directory ", dirName, " Created ")
+    else:
+        print("Directory ", dirName, " already exists")
+
+    os.chdir(dirName)
     # Making Weak Scaling Plot
     a = 0
     for i in range(len(Filt_model)):
@@ -134,36 +144,38 @@ def PlotCreator():
             temp_df = df1.loc[Filt_model[i]].loc[Filt_GPU_load[iter]]
             temp_df = temp_df.sort_values(by='Devices', ascending=True)
             if not temp_df.empty and len(temp_df['Devices']) > 4:
-                x = temp_df['Devices'] * temp_df['X'] * temp_df['Z'] / (temp_df['Total size'] / temp_df['Devices'])
+                '''x = temp_df['Devices'] * temp_df['X'] * temp_df['Z'] / (temp_df['Total size'] / temp_df['Devices'])
                 y = temp_df['Speed']
                 name = temp_df['Model'].unique()
-                # total_size = str((temp_df['Total size']/df1['Devices']).unique().tolist()[0])
-                make_plot_ghost(x, y, name[0] + str(a))
+                #total_size = str((temp_df['Total size']/df1['Devices']).unique().tolist()[0])
+                make_plot_ghost(x, y, name[0] + str(a))'''
+                make_plot_ghost(temp_df, str(a))
             a += 1
 
+    os.chdir("./")
 
 def make_plot_weak(x, y, name, size):
-    #fig = plt.plot(x, y, color='black', marker='x', markevery=1, markersize=7, linestyle=":", linewidth=2, label=f'bla')
-    fig=plt.plot(x,y, 'kx')
-    m, b  = np.polyfit(x,y,1)
-    x=np.linspace(0, np.amax(x)+1, 20)
-    plt.plot(x, m*x+b,  color='black', markevery=1, markersize=7, linestyle=":", linewidth=2, label=f'bla')
+    # fig = plt.plot(x, y, color='black', marker='x', markevery=1, markersize=7, linestyle=":", linewidth=2, label=f'bla')
+    fig = plt.plot(x, y, 'kx')
+    m, b = np.polyfit(x, y, 1)
+    x = np.linspace(0, np.amax(x) + 1, 20)
+    plt.plot(x, m * x + b, color='black', markevery=1, markersize=7, linestyle=":", linewidth=2, label=f'bla')
     plt.xlabel('Number of GPU', fontsize=24)
     plt.ylabel('MLBUps', fontsize=24)
     plt.ylim(ymin=0)
     plt.xlim(xmin=0)
     plt.xticks(np.arange(0, max(x) + 1, 1.0))
     plt.grid(True)
-    plt.savefig("Weak_scaling_" + name + "_" + size+"_2", dpi=600)
+    plt.savefig("Weak_scaling_" + name + "_" + size + "_2", dpi=600)
     plt.clf()
 
 
 def make_plot_strong(x, y, name, size):
     fig = plt.plot(x, y, color='black', marker='x', markevery=1, markersize=7, linestyle=":", linewidth=2, label=f'bla')
-    #fig = plt.plot(x, y, 'kx')
-    #m, b = np.polyfit(x, y, 1)
-    #x = np.linspace(0, np.amax(x) + 1, 20)
-    #plt.plot(x, m * x + b, color='black', markevery=1, markersize=7, linestyle=":", linewidth=2, label=f'bla')
+    # fig = plt.plot(x, y, 'kx')
+    # m, b = np.polyfit(x, y, 1)
+    # x = np.linspace(0, np.amax(x) + 1, 20)
+    # plt.plot(x, m * x + b, color='black', markevery=1, markersize=7, linestyle=":", linewidth=2, label=f'bla')
     plt.xlabel('Number of GPU', fontsize=24)
     plt.ylabel('MLBUps', fontsize=24)
     plt.ylim(ymin=0)
@@ -174,14 +186,51 @@ def make_plot_strong(x, y, name, size):
     plt.clf()
 
 
-def make_plot_ghost(x, y, name):
-    fig = plt.plot(x, y, 'gx')
+def make_plot_ghost(df, a):
+    fig, ax = plt.subplots()
+    A_V = df['Devices'] * df['X'] * df['Z'] / (df['Total size'] / df['Devices'])
+    df2=pd.concat([df, A_V], axis=1)
+    df2 = df2.rename(columns={0: 'A/V'})
+
+    df2 = df2.sort_values(by='A/V', ascending=True)
+
+    temp_df = df2.loc[df2['Devices'] == 1]
+    if not temp_df.empty:
+        x1 = temp_df['A/V']
+        y1 = temp_df['Speed']
+        line1 = ax.plot(x1, y1, color="black", marker="x", markevery=1, markersize=7, linestyle="-", linewidth=2,
+                    label=f'1 GPU')
+
+    temp_df = df2.loc[df2['Devices'] == 2]
+    if not temp_df.empty:
+        x2 =temp_df['A/V']
+        y2 = temp_df['Speed']
+        line2 = ax.plot(x2, y2, color="black", marker=">", markevery=1, markersize=7, linestyle=":", linewidth=2,
+                        label=f'2 GPU')
+
+    temp_df = df2.loc[df2['Devices'] == 4]
+    if not temp_df.empty:
+        x3 = temp_df['A/V']
+        y3 = temp_df['Speed']
+
+        line3 = ax.plot(x3, y3, color="black", marker="<", markevery=1, markersize=7, linestyle="-", linewidth=2,
+                    label=f'4 GPU')
+
+    temp_df = df2.loc[df2['Devices'] == 8]
+    if not temp_df.empty and len(temp_df['Devices']) > 4:
+        x4 = temp_df['Devices'] * temp_df['X'] * temp_df['Z'] / (temp_df['Total size'] / temp_df['Devices'])
+        y4 = temp_df['Speed']
+        line4 = ax.plot(x4, y4, color="black", marker="v", markevery=1, markersize=7, linestyle=".", linewidth=2,
+                    label=f'8 GPU')
+
+    ax.legend()
     plt.title('Speed in function of A/V', fontsize=32)
     plt.xlabel('A/V', fontsize=24)
     plt.ylabel('MLBUps', fontsize=24)
     plt.ylim(ymin=0)
     plt.grid(True)
-    plt.savefig("AV_plot_" + name, dpi=600)
+    name = df['Model'].unique()
+    plt.savefig("AV_plot_" + name[0]+a, dpi=600)
     plt.clf()
 
 
